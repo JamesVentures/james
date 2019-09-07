1bef2bec31687c9a43d31db66897046fb8b1d38a
James Prestwich <james@prestwi.ch>
2019-09-07 18:01:27 -0400

    reparaterize

--- deployment-params.js
+++ deployment-params.js
@@ -1,10 +1,13 @@
 // Deployment params
-module.exports.SUMMONER = ''
-module.exports.TOKEN = ''
+
+require('dotenv').config()
+
+module.exports.SUMMONER = process.env.SUMMONER
+module.exports.TOKEN = process.env.TOKEN
 module.exports.PERIOD_DURATION_IN_SECONDS = 17280
-module.exports.VOTING_DURATON_IN_PERIODS = 35
-module.exports.GRACE_DURATON_IN_PERIODS = 35
+module.exports.VOTING_DURATON_IN_PERIODS = 5
+module.exports.GRACE_DURATON_IN_PERIODS = 5
 module.exports.ABORT_WINDOW_IN_PERIODS = 5
-module.exports.PROPOSAL_DEPOSIT = '1000000000000000000' // Large numbers should be string or big numbers
+module.exports.PROPOSAL_DEPOSIT = '7000000000000000' // Large numbers should be string or big numbers
 module.exports.DILUTION_BOUND = 3
-module.exports.PROCESSING_REWARD = '10000000000'
+module.exports.PROCESSING_REWARD = '700000000'
